f7a078ca037ff90a483a26da9feb5f9cb48b3960
CJ Pais <cj@cjpais.com>
2022-01-06 16:30:07 -0800

    add deploy script

---
+++ packages/contracts/scripts/deploy.ts
@@ -0,0 +1,55 @@
+import fs from 'fs-extra';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Wallet } from '@ethersproject/wallet';
+import { XQSTRENDER__factory } from '../typechain';
+import util from 'util';
+import hre from 'hardhat';
+import 'hardhat-change-network';
+
+const exec = util.promisify(require('child_process').exec);
+
+async function start() {
+  const args = require('minimist')(process.argv.slice(2));
+
+  if (!args.chainId) {
+    throw new Error('--chainId chain ID is required');
+  }
+  const chainId = args.chainId;
+
+  const path = `${process.cwd()}/.env.${chainId}`;
+  const env = require('dotenv').config({ path }).parsed;
+  const provider = new JsonRpcProvider(env.RPC_ENDPOINT);
+  const wallet = new Wallet(`0x${env.PRIVATE_KEY}`, provider);
+  const addressesPath = `${process.cwd()}/addresses/${chainId}${
+    args.dev ? '-dev' : ''
+  }.json`;
+  const addressBook = JSON.parse(
+    await fs.readFileSync(addressesPath).toString()
+  );
+
+  hre.changeNetwork('mumbai');
+
+  if (!addressBook.render) {
+    console.log('Deploying exquisite graphics...');
+    const deployTx = await new XQSTRENDER__factory(wallet).deploy();
+    console.log('Deploy TX: ', deployTx.deployTransaction.hash);
+    await deployTx.deployed();
+    console.log('exquisite graphics deployed at ', deployTx.address);
+    addressBook.render = deployTx.address;
+    await fs.writeFile(addressesPath, JSON.stringify(addressBook, null, 2));
+    console.log('Waiting for more confirmations before verify…');
+    const tx = await deployTx.deployTransaction.wait(10);
+    console.log('Verifying contract...');
+    await hre.run('verify:verify', {
+      address: addressBook.render,
+      constructorArguments: []
+    });
+  }
+
+  console.log('Deployed!');
+}
+
+start().catch((e: Error) => {
+  console.error(e);
+  process.exit(1);
+});
