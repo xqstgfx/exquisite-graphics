0a54bcb20f17d4b26bd09d8f5cd79c10093a2cc8
CJ Pais <cj@cjpais.com>
2022-07-22 15:46:06 -0700

    comment out png

--- packages/js/src/utils/png.ts
+++ packages/js/src/utils/png.ts
@@ -2,45 +2,45 @@ import getPixels from 'get-pixels';
 import { getBinarySVG_Array } from '../api';
 
 import { PNG } from 'pngjs';
-import fs from 'fs';
+// import fs from 'fs';
 
 import { PixelBuffer } from '../ll_api';
 
-export const pngToPixels = async (img: string) => {
-  const pixels: { x: number; y: number; color: string }[] = [];
+// export const pngToPixels = async (img: string) => {
+//   const pixels: { x: number; y: number; color: string }[] = [];
 
-  var png = PNG.sync.read(fs.readFileSync(img));
-  if (png.width > 64 || png.height > 64) return pixels;
+//   var png = PNG.sync.read(fs.readFileSync(img));
+//   if (png.width > 64 || png.height > 64) return pixels;
 
-  let offset = 0;
-  for (let y = 0; y < png.height; y++) {
-    for (let x = 0; x < png.width; x++) {
-      pixels.push({
-        x,
-        y,
-        color: `#${png.data.readUInt32BE(offset).toString(16).padStart(8, '0')}`
-      });
-      offset += 4;
-    }
-  }
+//   let offset = 0;
+//   for (let y = 0; y < png.height; y++) {
+//     for (let x = 0; x < png.width; x++) {
+//       pixels.push({
+//         x,
+//         y,
+//         color: `#${png.data.readUInt32BE(offset).toString(16).padStart(8, '0')}`
+//       });
+//       offset += 4;
+//     }
+//   }
 
-  return pixels;
-};
+//   return pixels;
+// };
 
-export const pngToData = async (img: string) => {
-  const pixels = await pngToPixels(img);
+// export const pngToData = async (img: string) => {
+//   const pixels = await pngToPixels(img);
 
-  // create PixelBuffer
-  let buffer = getBinarySVG_Array(pixels);
+//   // create PixelBuffer
+//   let buffer = getBinarySVG_Array(pixels);
 
-  if (isError(buffer)) {
-    return;
-  } else {
-    // TODO, return err as a separate object in PixelBuffer
-    buffer = buffer as PixelBuffer;
-    return `${buffer.getPixelBuffer()}`;
-  }
-};
+//   if (isError(buffer)) {
+//     return;
+//   } else {
+//     // TODO, return err as a separate object in PixelBuffer
+//     buffer = buffer as PixelBuffer;
+//     return `${buffer.getPixelBuffer()}`;
+//   }
+// };
 
 const isError = function (e: any) {
   return (
